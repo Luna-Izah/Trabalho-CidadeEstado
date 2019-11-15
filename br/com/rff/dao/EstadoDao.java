e64c90b00cebfdebecb5075f78a433648fd766b1
Luna-Izah <31076911+Luna-Izah@users.noreply.github.com>
2019-11-14 21:09:51 -0300

    Add files via upload

---
+++ br/com/rff/dao/EstadoDao.java
@@ -0,0 +1,38 @@
+
+package br.com.rff.dao;
+
+import br.com.rff.contracts.ICriteria;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Erica
+ */
+public class EstadoDao extends AbstractDao {
+
+    @Override
+    protected String getTableName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Object> getByCriterios(ICriteria c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void salvar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remover(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
